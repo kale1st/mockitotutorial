29e977fd02b60008cb2914267cae2e32cbb7c40e
azizkale <azizkale@hotmail.com>
2023-10-10 12:28:50 +0200

    getEmployeeById, delte and update employee functions added

--- src/test/java/com/azizkale/mockitotutorial/service/EmployeeServiceTest.java
+++ src/test/java/com/azizkale/mockitotutorial/service/EmployeeServiceTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -18,6 +21,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.Date;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTest {
@@ -57,19 +61,82 @@ public class EmployeeServiceTest {
         Assertions.assertThat(savedEmployee).isNotNull();
     }
 
-   @Test
-public void EmployeeService_GetAllEmployee_ReturnsResponseDto(){
-    // Mocking a Page<Employee> using Mockito
-    Page<Employee> employees = Mockito.mock(Page.class);
+    @Test
+    public void EmployeeService_GetAllEmployee_ReturnsResponseDto(){
+        Page<Employee> employees = Mockito.mock(Page.class);
 
-    // Configuring the behavior of the employeeRepository mock
-    // When findAll method of employeeRepository is called with any Pageable, it should return the mocked Page<Employee>.
-    when(employeeRepository.findAll(Mockito.any(Pageable.class))).thenReturn(employees);
+        when(employeeRepository.findAll(Mockito.any(Pageable.class))).thenReturn(employees);
 
-   // Calling the findAll method of employeeService with page number 1 and page size 10
-    EmployeeResponse employeeResponse = employeeService.findAll(1, 10);
+        EmployeeResponse saveEmployee = employeeService.findAll(1,10);
 
-    // Asserting that the result (saveEmployee) is not null
-    Assertions.assertThat(employeeResponse).isNotNull();
-}
+        Assertions.assertThat(saveEmployee).isNotNull();
+    }
+
+    @Test
+    public void EmployeeService_GetEmployeeById_WhenEmployeeExists_ReturnsEmployeeOptional() {
+        // Arrange
+        int employeeId = 1;
+        Employee mockEmployee = Mockito.mock(Employee.class); // create a mock Employee
+        when(employeeRepository.findById(employeeId)).thenReturn(Optional.of(mockEmployee));
+
+        // Act
+        Optional<Employee> result = employeeService.findById(employeeId);
+
+        // Assert
+        assertTrue(result.isPresent()); // assert that the result is present
+        assertSame(mockEmployee, result.get()); // assert that the result is the same instance as the mockEmployee
+    }
+
+
+    @Test
+    public void EmployeeService_GetEmployeeById_WhenEmployeeNotExist_ReturnsOptionalEmpty(){
+        // Arrange
+        int employeeId = 2;
+        when(employeeRepository.findById(employeeId)).thenReturn(Optional.empty());
+
+        // Act
+        Optional<Employee> result = employeeService.findById(employeeId);
+
+        // Assert
+        assertFalse(result.isPresent()); // assert that the result is not present
+    }
+
+    @Test
+    public void EmployeeService_UpdateEmployee_ReturnsEmployeeDto(){
+        int employeeId=1;
+        Employee employee = Employee.builder()
+                .id(employeeId).name("Aziz KAle")
+                .dob(new Date()).gender("Male")
+                .department("Tesing").build();
+
+        EmployeeDto employeeDto = employeeService.mapToDto(employee);
+        when(employeeRepository.findById(employeeId)).thenReturn(Optional.ofNullable(employee));
+        when(employeeRepository.save(employee)).thenReturn(employee);
+
+        EmployeeDto updatedEmployee = employeeService.update(employeeDto,employeeId);
+
+        Assertions.assertThat(updatedEmployee).isNotNull();
+    }
+
+    @Test
+    public void EmployeeService_DeleteEmployee_ReturnsVoid() {
+        int employeeId = 1;
+        Employee employee = Employee.builder()
+                .id(employeeId).name("Aziz KAle")
+                .dob(new Date()).gender("Male")
+                .department("Tesing").build();
+
+        // When findById is invoked with the specified employeeId, it returns an Optional containing the employee.
+        when(employeeRepository.findById(employeeId)).thenReturn(Optional.ofNullable(employee));
+
+        // Configure the delete method to perform no action when called with an Employee object.
+        doNothing().when(employeeRepository).delete(employee);
+
+        // Invoke the delete method of the employeeService with the created employeeId.
+        employeeService.delete(employeeId);
+
+        // Use assertAll to ensure that no exceptions are thrown during the execution of the delete method.
+        assertAll(() -> employeeService.delete(employeeId));
+    }
+
 }
